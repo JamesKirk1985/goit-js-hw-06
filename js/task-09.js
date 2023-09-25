ee5e7ef5bc59629faed7eb6ca8153472210f8f19
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-09-26 02:38:32 +0300

    09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+const colorValue = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+button.addEventListener("click", colorFunction);
+function colorFunction() {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+}
+
+// console.dir(body);
+
+// console.log(getRandomHexColor());
