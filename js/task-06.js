41bdd0fcb03275b4fe77dee9cae7380adea518fb
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-09-26 01:30:32 +0300

    06 - 1

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,13 @@ const input = document.querySelector("#validation-input");
 input.addEventListener("blur", validatorFunction);
 function validatorFunction() {
   if (Number(input.value.length) === Number(input.dataset.length)) {
+    input.classList.remove("invalid");
     input.classList.add("valid");
-    return;
+    // return;
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
   }
-  input.classList.add("invalid");
   return;
 }
+console.dir(input);
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,3 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", formValidator);
+function formValidator() {}
