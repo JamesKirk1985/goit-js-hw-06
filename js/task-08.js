42078e013ebef8f5909e317cdd60e9ad50cbecba
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-09-26 02:20:11 +0300

    08

--- js/task-08.js
+++ js/task-08.js
@@ -1,3 +1,15 @@
 const form = document.querySelector(".login-form");
 form.addEventListener("submit", formValidator);
-function formValidator() {}
+const userDate = {};
+function formValidator(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля мають бути заповнені!");
+  } else {
+    userDate.email = email.value;
+    userDate.password = password.value;
+    console.log(userDate);
+  }
+  event.currentTarget.reset();
+}
