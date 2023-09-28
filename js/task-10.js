48e2f3fd04c33144b4373ed7ea7eb06cbda98c38
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-09-29 02:08:42 +0300

    all

--- js/task-10.js
+++ js/task-10.js
@@ -7,38 +7,28 @@ const controlButton = document.querySelector("#controls");
 const createButton = document.querySelector("button[data-create]");
 const destroyButton = document.querySelector("button[data-destroy]");
 const box = document.querySelector("#boxes");
-// createButton.addEventListener("click", createBoxes);
-// function createBoxes(amount) {
-//   amount = controlButton.children[0].value;
-//   for (let i = 0; i <= amount; i += 1) {
-//     box.insertAdjacentHTML(
-//       "afterbegin",
-//       `<div
-//   width="${30 + i * 10}"
-//   height="${30 + i * 10}"
-//   background-color="${getRandomHexColor()}">
-//   </div>`
-//     );
-//   }
-// }
+let amount = 0;
+controlButton.addEventListener("input", getValue);
+function getValue(event) {
+  amount = event.target.value;
+  console.log(event.target.value);
+  console.log(typeof event.target.value);
+}
 
-// const markup = `<div
-// width="${30 + i*10}"
-// height="${30 + i*10}"
-// background-color="${getRandomHexColor()}"></div>`;
-// box.insertAdjacentHTML("afterbegin", markup);
-
-console.log(controlButton.children[0].value);
-console.dir(controlButton.children[0]);
-// console.dir(createButton);
-
-// createBoxes(amount)
-// destroyBoxes()
-box.insertAdjacentHTML(
-  "afterbegin",
-  `<div 
-    width="30" 
-    height="30" 
-    background-color="${getRandomHexColor()}">
-    </div>`
-);
+createButton.addEventListener("click", createBoxes);
+function createBoxes() {
+  destroyBoxes();
+  const markup = [];
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    div.style.width = `${30 + i * 10}px`;
+    div.style.height = `${30 + i * 10}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    markup.push(div);
+  }
+  box.append(...markup);
+}
+destroyButton.addEventListener("click", destroyBoxes);
+function destroyBoxes() {
+  box.innerHTML = "";
+}
